b9cf3a33c9b7877f8b39eb12e8338d89072fde73
Chuck Cottrill <chuck@drata.com>
2021-06-09 21:09:42 -0400

    Remove schema documentation (moved to README.md) Remove redundant schema checks Add ping function (for convenience)

--- src/accounts.js
+++ src/accounts.js
@@ -11,72 +11,6 @@ import Joi from 'joi';
 import axios from 'axios';
 import handleError from './handleError';
 
-/*
-account schema = {
-    // Client credentials provided as part of your Partner Application.
-    client_id : string, // required
-    // Allows skipping of the /oauth/authorize call.
-    oauth_authorize : boolean, // default(false)
-    // Name of Account displayed in the Dashboard.
-    name : string, // required
-    // Fallback compliance city if candidate location is not provided.
-    default_compliance_city : string | null, // default(''), not required
-    // Fallback compliance state if candidate location is not provided. Format: ISO 3166-2:US.
-    default_compliance_state : string | null, // default(''), not required
-    // Permissible purpose to run background checks.
-    // Determines which background checks the Account is credentialed for.
-    // allowed values = [ "employment", "business", "insurance", "tenant" ]
-    purpose : string, // required
-    adverse_action_email : string, // default('')
-    support_email : string, // default('')
-    support_phone : string, // default('')
-    technical_contact_email : string(), // default('')
-    user : {
-      // Full name of the initial Admin user for the Account.
-      full_name : string, // required
-      // Email of the initial Admin user for the Account.
-      email : string, // required
-    }, // required
-    company : {
-      // Name of Company displayed in Checkr emails and branded web pages.
-      dba_name : string | null, // not required
-      // Industry that company operates in. Format: NAICS 2017 Code.
-      industry : string | null, // not required
-      // City where company is headquartered.
-      city : string, // required
-      // State where company is headquartered. Format: ISO 3166-2:US.
-      state : string, // required
-      // Street address where company is headquartered.
-      street : string, // required
-      // Company Tax ID number.
-      tax_id : string, // required
-      // Zipcode where company is headquartered.
-      zipcode : string, // required
-      // State where company is incorporated. Format: ISO 3166-2:US.
-      incorporation_state : string | null, // not required
-      // Type of incorporation. (incorporation_type : string)
-      // incorporation_values = [
-      //   "association",
-      //   "co-ownership",
-      //   "corporation",
-      //   "joint-venture",
-      //   "limited-partnership",
-      //   "llc",
-      //   "llp",
-      //   "non-profit",
-      //   "partnership",
-      //   "s-corporation",
-      //   "sp",
-      //   "trusteeship" ]
-      incorporation_type : string, // required().allow(incorporation_values),
-      // Company phone number.
-      phone : string | null, // not required
-      // Company website.
-      website : string | null, // not required
-    }, // required
-  };
-*/
-
 const accounts = options => {
   return {
     create: async params => {
@@ -97,34 +31,6 @@ const accounts = options => {
         "s-corporation",
         "sp",
         "trusteeship" ];
-      const schema_user = Joi.object().keys({
-        full_name : Joi.string().min(8).default(''),
-        email : Joi.string().min(6).default(''),
-      });
-      const schema_company = Joi.object().keys({
-        // Name of Company displayed in Checkr emails and branded web pages.
-        dba_name : Joi.string().default(''), // not required
-        // Industry that company operates in. Format: NAICS 2017 Code.
-        industry : Joi.string().default(''), // not required
-        // City where company is headquartered.
-        city : Joi.string().min(4).required(),
-        // State where company is headquartered. Format: ISO 3166-2:US.
-        state : Joi.string().min(2).required(),
-        // Street address where company is headquartered.
-        street : Joi.string().required(),
-        // Company Tax ID number.
-        tax_id : Joi.string().min(8).required(),
-        // Zipcode where company is headquartered.
-        zipcode : Joi.string(), // required(),
-        // State where company is incorporated. Format: ISO 3166-2:US.
-        incorporation_state : Joi.string().min(2).default(''), // not required
-        // Type of incorporation. allow: incorporation_values
-        incorporation_type : Joi.string().required().allow(incorporation_values),
-        // Company phone number.
-        phone : Joi.string(), // not required
-        // Company website.
-        website : Joi.string(), // not required
-      });
       const schema = Joi.object().keys({
         // Client credentials provided as part of your Partner Application.
         client_id: Joi.string().min(8).required(),
@@ -180,14 +86,6 @@ const accounts = options => {
       if (validation.error !== null) {
         throw new Error(validation.error);
       }
-      const validation_user = Joi.validate(params.user, schema_user);
-      if (validation_user.error !== null) {
-        throw new Error(validation_user.error);
-      }
-      const validation_company = Joi.validate(params.company, schema_company);
-      if (validation_company.error !== null) {
-        throw new Error(validation_company.error);
-      }
       if ( !( incorporation_values.includes(params.company.incorporation_type) ) ) {
         // console.log("incorporation_type:",params.company.incorporation_type);
         throw new Error(`Invalid parameter, incorporation type`);
@@ -219,6 +117,21 @@ const accounts = options => {
         });
         // console.log("account:",res.data)
         return res.data;
+      } catch (error) {
+        handleError(error);
+      }
+    },
+    ping: async () => {
+      try {
+        const res = await axios({
+          method: 'get',
+          url: `${options.baseUrl}/${options.apiVersion}/account`,
+          auth: {
+            username: options.apiKey,
+            password: ''
+          }
+        });
+        return res;
       } catch (error) {
         handleError(error);
       }
